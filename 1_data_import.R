574872c066e64bfb3e717324c4a09f581b57e543
MiksLynx <miks.lusitis@gmail.com>
2018-07-14 21:39:55 +0300

    Initial commit with full data import.

---
+++ 1_data_import.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+application_train <- read_csv('datasets/application_train.csv')
+bureau <- read_csv('datasets/bureau.csv')
+bureau_balance <- read_csv('datasets/bureau_balance.csv')
+credit_card_balance <- read_csv('datasets/credit_card_balance.csv')
+installments_payments <- read_csv('datasets/installments_payments.csv')
+pos_cash_balance <- read_csv('datasets/POS_CASH_balance.csv')
+previous_application <- read_csv('datasets/previous_application.csv')
+
+column_descriptions <- read_csv('datasets/HomeCredit_columns_description.csv')
+
+
